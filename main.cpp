0d9415ed9a751d367b09229bad4216a1dc4c535f
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-12-13 23:09:52 +0800

    feat: implement messages functionality

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ struct Message {
 };
 
 User users[MAX_USERS];
+Message u_messages[MAX_MESSAGES];
 
 User empty_user;
 User *current_user = &empty_user;
@@ -31,6 +32,8 @@ bool create_message(string content, string receiver);
 
 int find_empty_user();
 int find_user(string username);
+
+int find_empty_message();
 
 int main() {
     return 0;
@@ -90,6 +93,23 @@ bool delete_user(string username) {
     return true;
 }
 
+bool create_message(string content, string receiver) {
+    int m_index = find_empty_message();
+    if (m_index == -1) {
+        return false;
+    }
+
+    int u_index = find_user(receiver);
+    if (u_index == -1) {
+        return false;
+    }
+
+    u_messages[m_index].sender = current_user;
+    u_messages[m_index].receiver = &users[u_index];
+    u_messages[m_index].content = content;
+    return true;
+}
+
 // HELPER FUNCTIONS
 int find_user(string username) {
     for (int i = 0; i < MAX_USERS; i++) {
@@ -103,6 +123,15 @@ int find_user(string username) {
 int find_empty_user() {
     for (int i = 0; i < MAX_USERS; i++) {
         if (users[i].username == "") {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int find_empty_message() {
+    for (int i = 0; i < MAX_MESSAGES; i++) {
+        if (u_messages[i].content == "") {
             return i;
         }
     }
